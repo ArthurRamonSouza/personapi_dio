aee73e2eb8a88bff96fb8fce15d9073553197038
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2022-01-30 20:44:04 -0300

    Java 1.8, adicionando as classes Person, Phone e  PhoneType

---
+++ src/main/java/me/dio/web/personapi/model/PhoneType.java
@@ -0,0 +1,14 @@
+package me.dio.web.personapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+	HOME,
+	MOBILE,
+	COMMERCIAL;
+	
+}
