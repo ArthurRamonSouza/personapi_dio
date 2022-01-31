b910f2a94d63c863d7a430872f7876d3c248cb06
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2022-01-31 12:34:46 -0300

    Java 1.8, classes adicionadas: Person, PhoneType, Phone.

--- src/main/java/me/dio/web/personapi/controller/PersonController.java
+++ src/main/java/me/dio/web/personapi/controller/PersonController.java
@@ -1,4 +1,4 @@
-package me.dio.web.personapi;
+package me.dio.web.personapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
